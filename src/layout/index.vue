1398e4965ba311f7d8882b1d5b6454f33db96451
XiaoXue <1006854291@qq.com>
2020-11-30 18:03:59 +0800

    布局 开始

---
+++ src/layout/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>layout
+
+      <div></div>
+      <div></div>
+  </div>
+</template>
+
+<script>
+import index from "./minxi/index";
+export default {
+  name: "Layout",
+  mixins: [index],
+};
+</script>
+
+<style>
+</style>
